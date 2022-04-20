a5b6e67dd7b22b410578029e770603a9ee63d51d
marcomtl <97364697+marcomtl@users.noreply.github.com>
2022-04-20 17:40:46 -0400

    Add files via upload

---
+++ actionpage.php
@@ -0,0 +1,10 @@
+if (isset($_POST['submit'])) {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $country = $_POST['country'];
+    $name = $_POST['subject'];
+
+    $mailTo = ""
+    $headers = "From: My website"
+    header("location: action.php")
+}
